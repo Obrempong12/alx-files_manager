4c826891b0b66be6569505a248f311ec07e58357
Obrempong12 <tuntumbrane@gmail.com>
2024-12-01 07:13:44 -0800

    Edited

--- worker.js
+++ worker.js
@@ -14,7 +14,7 @@ const userQueue = new Queue('email sending');
 /**
  * Generates the thumbnail of an image with a given width size.
  * @param {String} filePath The location of the original file.
- * @param {number} size The width of the thumbnail.
+ * @param {number} size The width of the thumbnail
  * @returns {Promise<void>}
  */
 const generateThumbnail = async (filePath, size) => {
